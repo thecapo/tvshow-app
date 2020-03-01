58fe0a1ef8259b0a609d5739375943bdb8660e51
thecapo <the7thcapo@gmail.com>
2020-03-01 21:52:09 +1100

    Updates

---
+++ precache-manifest.35566d2f8017fe0e2b26e963e50cb7b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c74f7fbba07e2e7bac498ad7740be11",
+    "url": "/tv-app/index.html"
+  },
+  {
+    "revision": "bdb626fda347c7da284b",
+    "url": "/tv-app/static/css/main.7544c121.chunk.css"
+  },
+  {
+    "revision": "40abc448c605c3908bf4",
+    "url": "/tv-app/static/js/2.e4d53d92.chunk.js"
+  },
+  {
+    "revision": "bdb626fda347c7da284b",
+    "url": "/tv-app/static/js/main.4109f0be.chunk.js"
+  },
+  {
+    "revision": "c3abf15c0b5dfb1341f1",
+    "url": "/tv-app/static/js/runtime~main.0a23bb06.js"
+  }
+]);
