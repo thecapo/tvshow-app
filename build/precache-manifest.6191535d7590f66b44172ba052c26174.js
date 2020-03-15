823186bdfd96560337cbe15306db356b3c0c0c66
thecapo <the7thcapo@gmail.com>
2020-03-15 18:13:35 +1100

    commit

---
+++ build/precache-manifest.6191535d7590f66b44172ba052c26174.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3427022f20053b9e61060fb13a6e85e3",
+    "url": "/tvApp/index.html"
+  },
+  {
+    "revision": "e8f5a04d5d9e273b2028",
+    "url": "/tvApp/static/css/main.7544c121.chunk.css"
+  },
+  {
+    "revision": "baa7b171ed3cdfc56377",
+    "url": "/tvApp/static/js/2.04b68dbf.chunk.js"
+  },
+  {
+    "revision": "e8f5a04d5d9e273b2028",
+    "url": "/tvApp/static/js/main.b8475a5a.chunk.js"
+  },
+  {
+    "revision": "3b93b85d74fd9c3672e0",
+    "url": "/tvApp/static/js/runtime~main.78134031.js"
+  }
+]);
