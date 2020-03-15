c8edd23bedf91463dd02e40a5632d2aca0af5886
thecapo <the7thcapo@gmail.com>
2020-03-15 17:56:37 +1100

    publish app on github

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tvshow-app/precache-manifest.7bcaa74878daff43923fb2daa95160c8.js"
+  "/precache-manifest.7c7b733658bcfd15c6a4385a47a9b005.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tvshow-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
